8cc142d8a37009bbb6e7ad36e90ebfdc14ccfa43
Ced02-ETH <codezcee@gmail.com>
2025-11-22 16:34:49 +0000

    Adjusted the models file

---
+++ Backend/app/models.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel 
+from typing  import Literal ,Optional ,List
+
+class UserSettings(BaseModel):
+    difficulty:Literal["very simple","simple","normal","detailed" ]
+    profile: List [Literal["dyslexia","dysgraphia","dyscalculia","hearing_impairment"]]
+
+class TranscriptChunk(BaseModel):
+    session_id:str
+    text:str
+    start_time: float
+    end_time:float
+class SummaryPayload(BaseModel):
+    sesseion_id:str
+    simple_txt:str
+    expanded_text:str
+    notes_for_hearing:str
+
+     
